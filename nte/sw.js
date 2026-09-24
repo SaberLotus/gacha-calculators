1b70b445c77afb5fe73f4d0915cbbee07049a04e
SaberLotus <tlpmontip452637@gmail.com>
2026-09-24 10:19:59 +0700

    Keep NTE offline cache isolated

--- nte/sw.js
+++ nte/sw.js
@@ -2,7 +2,7 @@
 const CACHE = 'nte-v1';
 const SHELL = ['./', './index.html', './manifest.json', './icon.svg'];
 self.addEventListener('install', e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(SHELL)).then(() => self.skipWaiting())));
-self.addEventListener('activate', e => e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE && !k.startsWith(CACHE + '-fonts')).map(k => caches.delete(k)))).then(() => self.clients.claim())));
+self.addEventListener('activate', e => e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k.startsWith('nte-') && k !== CACHE && !k.endsWith('-fonts')).map(k => caches.delete(k)))).then(() => self.clients.claim())));
 self.addEventListener('fetch', e => {
   const req = e.request;
   if (req.method !== 'GET') return;
